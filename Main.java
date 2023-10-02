fc0b8dfff67574cdb1153e2a37bac6f815c61899
Elizaveta Kovaleva <kovaleva.li.pr@gmail.com>
2023-10-02 19:02:11 -0400

    initial commit

---
+++ Main.java
@@ -0,0 +1,31 @@
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.util.List;
+
+public class Main {
+
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader bufferedReader = new BufferedReader(new FileReader("input.csv"));
+
+        String readLine = bufferedReader.readLine();
+
+        System.out.println(readLine);
+
+
+        List<String> allLines = Files.readAllLines(Path.of("input.csv"));
+
+
+        for (String eachLine
+                : allLines) {
+
+            System.out.println(eachLine);
+
+        }
+
+    }
+}
